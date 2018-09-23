a351db877b9aa228f1e016fc850bfffa9ee449c5
rogkesavan <skesavane@gmail.com>
2018-09-23 20:07:51 +0530

    Solving the environment without reinforcement learning

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ state = envout.encode(3, 1, 2, 0) # (taxi row, taxi column, passenger index, des
 envout.s = state
 envout.render()
 envout.P[328]
+
+epoch = 0
+penalties, reward = 0, 0
+frames = [] #for anniamation 
+done = False 
+while not done:
+    action = envout.action_space.sample()
+    state, reward, done, info = envout.step(action)
+    if reward == -10:
+        penalties += 1
 print("Action Space {}".format(envout.action_space))
 print("State Space {}".format(envout.observation_space))
 print("State:", state)
